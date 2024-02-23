3c134281bd8d96425df81e8e35dc4769823c8a71
rahulkundelwalll <pooransherrahul@gmail.com>
2024-02-24 00:20:25 +0530

    24Feb

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import store from './Redux/Store';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import store from "./Redux/Store";
+import { Provider } from "react-redux";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-  <App />
-</Provider>
+    <App />
+  </Provider>
 );
+// comment
